73fc3c58324da0e17faea556509fd756e1d78fa9
Juan DeBiedma <juanthefatty@gmail.com>
2017-03-15 22:47:58 -0400

    first room-making feature, name add property

--- logic.js
+++ logic.js
@@ -19,34 +19,86 @@ var connectionsRef = database.ref("/connections");
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = database.ref(".info/connected");
 
-var currentUsers = 1;
+var playersRef = database.ref("/players");
 
-connectedRef.on("value", function(snap) {
+var playerCount = 0;
 
-  // If they are connected..
-  if (snap.val()) {
-
-  	if (currentUsers < 2) {
-
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
+var entered = false;
 
 
-
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-	}
-  }
-});
 
 connectionsRef.on("value", function(snap) {
 
 	currentUsers = snap.numChildren();
-	console.log(currentUsers);
+
+	
 
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
   $("#viewerCount").html("viewers: " + snap.numChildren());
 });
 
-database.ref("/")
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+
+
+  if (snap.val()) {
+
+    // Add user to the connections list.
+
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+$("#joinButton").on("click", function(){
+
+var playerName = $("#name-input").val();
+
+if (playerCount < 2 && !entered) {
+
+var adder = playersRef.push({
+
+		
+			name : playerName,
+			wins: 0,
+			losses: 0        
+				
+      });
+
+adder.onDisconnect().remove();
+
+
+
+$("#waitingRoom").html("<h5 class='animate-flicker'>waiting for opponent...</h5><br><br>")
+.append("<img id='loadingPic' src='https://2aih25gkk2pi65s8wfa8kzvi-wpengine.netdna-ssl.com/gmat/wp-content/plugins/magoosh-lazyload-comments-better-ui/assets/img/loading.gif'>");
+
+$("#name-input").val("");
+
+$("#joinButton").slideUp();
+$("#firstInput").slideUp();
+
+
+entered = true;
+
+}
+
+else {alert("Room is full!")};
+
+});
+
+playersRef.on("value", function(snap) {
+
+console.log(snap.numChildren());
+
+playerCount = snap.numChildren();
+
+})
+
+
+
+
+
